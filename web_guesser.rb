3d30053b6abf2248cf8095cbb64a7d9c74774134
sunsplat <heeycutie@yahoo.com>
2015-12-01 11:04:48 +0800

    adding limited number of guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,6 +4,7 @@ require 'sinatra/reloader'
 number = rand(100)
 
 get '/' do
+	@@guesses = 5
   guess = params["guess"]
   message = check_guess(guess, number)
  	color = change_background(message)
@@ -20,7 +21,8 @@ def check_guess(guess, number)
 	elsif number-5 > guess.to_i
 		"WAAAY too low!"
 	elsif guess.to_i == number
-		"You got it right! The secret number is #{number}"	
+		"You got it right! The secret number is #{settings.number}"
+		new_game
 	end
 end
 
@@ -36,6 +38,22 @@ def change_background(message)
 	else
 		color = 'green'
 	end
-		
+	
 end
+
+def check_guesses(guess)
+	if @@guesses == 0
+		new_game
+		"You're out of guesses. The number is #{@number}"
+	else
+		@@guesses -= 1
+		check_guess(guess)
+	end
+end
+
+def new_game
+	number = rand(100)
+	@@guesses = 5
+end
+
 
