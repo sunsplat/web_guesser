d9f311307e3cdf1b170514105619b4c697e1ea7e
sunsplat <heeycutie@yahoo.com>
2015-12-01 09:51:35 +0800

    change background according to guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,7 +6,8 @@ number = rand(100)
 get '/' do
   guess = params["guess"]
   message = check_guess(guess, number)
-  erb :index, :locals => {:number => number, :guess => guess, :message => message}
+ 	color = change_background(message)
+  erb :index, :locals => {:number => number, :color => color, :message => message}
 end
 
 def check_guess(guess, number)
@@ -19,7 +20,22 @@ def check_guess(guess, number)
 	elsif number-5 > guess.to_i
 		"WAAAY too low!"
 	elsif guess.to_i == number
-		"You got it right! The secret number is #{number}"		
+		"You got it right! The secret number is #{number}"	
 	end
+end
+
+def change_background(message)
+	if message == "WAAAY too high!"
+		color = 'red'
+	elsif message == "Too high!"
+		color = '#FFC0CB'
+	elsif message == "Too low!"
+		color = '#FFC0CB'
+	elsif message == "WAAAY too low!"
+		color = 'red'
+	else
+		color = 'green'
+	end
+		
 end
 
